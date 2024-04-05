5bd4313c538747a33c7ce886040e956c198b1d97
akrherz <akrherz@iastate.edu>
2024-04-05 16:23:10 -0500

    mnt: improve coverage

--- tests/test_service.py
+++ tests/test_service.py
@@ -2,8 +2,9 @@
 
 import os
 
+import mock
 import pytest
-from TileCache.Service import Service
+from TileCache.Service import Service, wsgiHandler
 
 
 @pytest.fixture
@@ -17,3 +18,16 @@ def service():
 def test_generate_crossdomain_xml(service):
     """Exercise API."""
     assert service.generate_crossdomain_xml() is not None
+
+
+def test_wsgi_handler(service):
+    """Exercise API."""
+    env = {
+        "QUERY_STRING": "",
+        "PATH_INFO": "/1.0.0/profit2015/0/0/0.jpg",
+        "REQUEST_METHOD": "GET",
+        "SCRIPT_NAME": "tile.py",
+        "wsgi.input": mock.MagicMock(),
+    }
+    sr = mock.MagicMock()
+    assert wsgiHandler(env, sr, service) is not None
